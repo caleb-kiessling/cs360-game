b54b7e89ef410951b36fc1bf8c0359e0e6306062
MasonLewis04 <lewima03@pfw.edu>
2024-12-10 16:42:40 -0500

    working ship controls and bullet animation
    Ship controls are working they need to be tuned. A method was added to fire bullets.

--- Game/src/main/java/controllers/GameController.java
+++ Game/src/main/java/controllers/GameController.java
@@ -5,9 +5,12 @@ import java.awt.event.KeyListener;
 import application.core.GameLoop;
 import application.core.GameObject;
 import application.entities.Spaceship;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
@@ -26,41 +29,86 @@ public class GameController extends BaseController implements KeyListener {
     @FXML
     private Label AmmoLabel;
     
+    private int ammo;
+    
 //Set up key listener on initialization
     @FXML
 	public void initialize() {
+    	ammo=5;
+    	AmmoLabel.setText("["+ammo+"] SHOTS");
+    	player = new Spaceship();
+        GameContent.getChildren().add(player.getVisual());
+        player.setXY(300,300);
     	GameContent.sceneProperty().addListener((obs, oldScene, newScene) -> {
             if (newScene != null) {
                 newScene.setOnKeyPressed(this::moveShip);
             }
         });
+    	System.out.print(GameContent.getChildren());
+    	System.out.print(player.getParent());
+    	
     	
 	}
-    //method for moving the ship. Currently does not function correctly
+    //method for ship actions and key events
     @FXML
     void moveShip(KeyEvent e) {
-    	double x = player.getLayoutX();
-        double y = player.getLayoutY();
-        double step=20;
-        //used for debugging will show key pressed and coordinate but ship visually does not move to the coordinate
-        System.out.println("Key pressed: " + e.getCode()); // Check key event
-        System.out.println("Before move: X=" + x + ", Y=" + y);
+    	double x = player.getVisual().getLayoutX();
+        double y = player.getVisual().getLayoutY();
+        double step=15;
+        //used for debugging will show key pressed and coordinate
+        //System.out.println("Key pressed: " + e.getCode()); // Check key event
+       // System.out.println("Before move: X=" + x + ", Y=" + y);
 
         if (e.getCode() == KeyCode.UP) {
-            player.setLayoutY(y - step);
+            player.getVisual().setLayoutY(y - step);
         }
         if (e.getCode() == KeyCode.DOWN) {
-            player.setLayoutY(y + step);
+            player.getVisual().setLayoutY(y + step);
         }
         if (e.getCode() == KeyCode.LEFT) {
-            player.setLayoutX(x - step);
+            player.getVisual().setLayoutX(x - step);
         }
         if (e.getCode() == KeyCode.RIGHT) {
-            player.setLayoutX(x + step);
+            player.getVisual().setLayoutX(x + step);
         }
-        System.out.println("After move: X=" + player.getLayoutX() + ", Y=" + player.getLayoutY());
+        if (e.getCode() == KeyCode.SPACE) {
+            shoot();
+        }
+        
+        System.out.println("After move: X=" + player.getVisual().getLayoutX() + ", Y=" + player.getVisual().getLayoutY());
 
    }
+    //method that creates that shape for bullet and play animation of bullet firing
+    @FXML
+    void shoot() {
+    	if(ammo>0) {
+    		ammo--;
+        	AmmoLabel.setText("["+ammo+"] SHOTS");
+        	Rectangle bullet=new Rectangle(5,20);
+            GameContent.getChildren().add(bullet);
+            bullet.setX(300);//setX and setY need to be the same as ship to spawn in it
+            bullet.setY(300);
+            bullet.setLayoutX(player.getVisual().getLayoutX());
+            bullet.setLayoutY(player.getVisual().getLayoutY());
+            //print statement for debugging
+            System.out.println("Bullet at: X=" + bullet.getLayoutX() + ", Bullet at:" + bullet.getLayoutY());
+            //animation of bullet
+            TranslateTransition shootBullet=new TranslateTransition();
+            shootBullet.setDuration(Duration.seconds(2)); // Animation duration
+            shootBullet.setNode(bullet); // The node to animate
+            shootBullet.setByY(-1000); // Move 1000px up
+            shootBullet.setCycleCount(1); // play animation once
+            shootBullet.setAutoReverse(false); // Reverse direction after each cycle
+            shootBullet.setOnFinished(event -> {//used to destroy bullet object once animation is finished
+            	GameContent.getChildren().remove(bullet);
+            });
+
+            // Start the animation
+            shootBullet.play();
+    	}
+    	
+
+    }
  
     public void addGameObject(GameObject gameObject) {
         gameLoop.addGameObject(gameObject);
@@ -98,18 +146,18 @@ public class GameController extends BaseController implements KeyListener {
         
         System.out.print("GameCOntroller initialize()");
         System.out.print(GameContent.getChildren().size());
-        player = new Spaceship();
-        GameContent.getChildren().add(player);
-        int playerIndex = GameContent.getChildren().indexOf(player);
-        GameContent.getChildren().get(playerIndex).setLayoutX(20);
-        GameContent.getChildren().get(playerIndex).setLayoutY(20);
-        GameContent.getChildren().get(playerIndex).setVisible(true);
+        //player = new Spaceship();
+       // GameContent.getChildren().add(player);
+      //  int playerIndex = GameContent.getChildren().indexOf(player);
+        //GameContent.getChildren().get(playerIndex).setLayoutX(20);
+       // GameContent.getChildren().get(playerIndex).setLayoutY(20);
+       // GameContent.getChildren().get(playerIndex).setVisible(true);
         GameContent.setVisible(true);
         
         gameLoop.start();
-        player = new Spaceship();
-        player.setXY(300.0, 300.0);
-        GameContent.getChildren().add(player.getVisual());
+      //  player = new Spaceship();
+      //  player.setXY(300.0, 300.0);
+       // GameContent.getChildren().add(player.getVisual());
         
         //.addKeyListener();
     }
